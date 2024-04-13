9b709eb5468c55882ab2c5c966add3c31b0b44e2
NezhelskiyViktor <nezhelskiy@yandex.ru>
2024-04-13 11:42:51 +0300

    add all cycle code

--- main.py
+++ main.py
@@ -4,23 +4,38 @@
 import pygame
 import random
 
+pygame.init()
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
-pygame.init()
-pygame.display.set_caption('Игра Тир')
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption('Игра Тир')
 icon = pygame.image.load('images/icon.jpg')
 pygame.display.set_icon(icon)
-target_image = pygame.image.load('images/00_00001553_1-800x800.jpg')
+
+target_img = pygame.image.load('images/target.jpg')
 target_width = 200
 target_height = 200
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
 color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update
+
 
 pygame.quit()
